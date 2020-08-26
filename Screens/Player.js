d5214f5d44e125528ea3f5ca42a930f9c44ede5d
joeyalpha5 <1603898@students.wits.ac.za>
2020-08-26 11:38:06 +0200

    video player working - stable

--- Screens/Player.js
+++ Screens/Player.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Dimensions} from 'react-native';
 import Video from 'react-native-video';
 export const Player = ({navigation, route}) =>{
     const data = route.params.data;
+    const {width, height} = Dimensions.get("screen");
     useEffect(() => {
         console.log(route.params.data);
     });
@@ -11,8 +12,8 @@ export const Player = ({navigation, route}) =>{
             <Video source={{uri: data.post_source_url }} 
                 repeat={true}       
                 ignoreSilentSwitch={"obey"}                          
-                resizeMode={"cover"}           
-                style={{width:width,height:height/2}} />
+                resizeMode={"contain"}           
+                style={{width:width,height:height/3, backgroundColor:'#000000'}} />
         </View>
     )
 }
